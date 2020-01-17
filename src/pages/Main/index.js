7d8ca5c1ec96a3da915b3b77384743887ffbdd9a
Italo M <italomarcos0010@gmail.com>
2020-01-17 15:05:04 -0300

    feature: fetching api data

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,55 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
+import { Container, Form, Input, SubmitButton } from './styles';
 
-import { Container } from './styles';
+Icon.loadFont();
 
-export default function Main() {
-  return <View />;
+export default class Main extends Component {
+  state = {
+    users: [],
+    newUser: '',
+  };
+
+  handleSend = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/user/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({ users: [...users, data], newUser: '' });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { newUser } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Add repository"
+            value={newUser}
+            onChangeText={t => this.setState({ newUser: t })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleSend}
+          />
+          <SubmitButton onPress={this.handleSend}>
+            <Icon name="add" size={20} color="black" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,34 @@
-import { styled } from 'styled-components/native';
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1; /* ocupa todo o espaço possível da tela */
   padding: 30px; /* distancia das laterais (up, bottom , l & r) */
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px; /* borda de baixo */
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#666',
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #e6b32a;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 12px;
 `;
